67c31e74359704416f7fb02cb6a08ec64d37cac8
Alexa <kusteralexa@gmail.com>
2022-11-26 09:55:02 -0500

    commit tree 25db5b2a225dac7ddd61d1c231093c533840fd72

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= 42) {
+        return (someValue - 42);
+    } else {
+        return (42 - someValue);
+
+    }
+
+}
